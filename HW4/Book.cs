89d3c48be3cfbb922d7193e43a2d6a9499458b91
H_Epc <H_Epc@HAPPYEUGENEPC>
2022-12-18 12:54:30 +0200

    Добавьте файлы проекта.

---
+++ HW4/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    internal class Book
+    {
+        //[JsonIgnore]
+        public int PublishingHouseId { get; set; }
+        //[JsonPropertyName("Name")]
+        public string Title { get; set; }
+        public PublishingHouse PublishingHouse { get; set; }
+
+    }
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,6 @@
+﻿using HW4;
+using System.Text.Json;
+
+string path = "./JSON.json";
+var content = File.ReadAllText(path);
+var deserializedJson = JsonSerializer.Deserialize<List<Book>>(content);
